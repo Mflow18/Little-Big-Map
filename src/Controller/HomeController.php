409692872158024165f9ed8d3bf65aa4ccb860f2
Mathieu Florentin <mathieu.florentin27@gmail.com>
2019-12-18 21:59:56 +0100

    CSS done

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/creeruncompte", name="createaccount")
+     */
+    public function createaccount(Request $request, UserPasswordEncoderInterface $encoder): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $plainpassword = $user->getPassword();
+            $encodedPassword = $encoder->encodePassword($user, $plainpassword);
+            $user->setPassword($encodedPassword);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('success', [
+                'user' => $user,
+            ]);
+        }
+
+        return $this->render('/home/createaccount.html.twig', [
+            'controller_name' => 'HomeController',
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/UserType.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Age;
+use App\Entity\Category;
+use App\Entity\Children;
+use App\Entity\Family;
+use App\Entity\Iris;
+use App\Entity\Profession;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+            ])
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('picture', TextType::class)
+            ->add('professions', EntityType::class,
+                [
+                    'class' => Profession::class,
+                    'choice_label' => 'name',
+                    'expanded' => true,
+                    'multiple' => true,
+                    'by_reference' => false,
+                ])
+            ->add('age', EntityType::class,
+                [
+                    'class' => Age::class,
+                    'choice_label' => 'ageRange',
+                    'expanded' => true,
+                    'multiple' => true,
+                    'by_reference' => false,
+                ]
+            )
+            ->add('children', EntityType::class,
+                [
+                    'class' => Children::class,
+                    'choice_label' => 'number',
+                    'expanded' => true,
+                    'multiple' => true,
+                    'by_reference' => false,
+                ])
+            ->add('categories', EntityType::class,
+                [
+                    'class' => Category::class,
+                    'choice_label' => 'name',
+                    'expanded' => true,
+                    'multiple' => true,
+                    'by_reference' => false,
+                ]
+            )
+            ->add('family', EntityType::class,
+                [
+                    'class' => Family::class,
+                    'choice_label' => 'state',
+                    'expanded' => true,
+                    'multiple' => true,
+                    'by_reference' => false,
+                ]
+            )
+            ->add('irises', EntityType::class,
+                [
+                    'class' => Iris::class,
+                    'choice_label' => 'name',
+                    'expanded' => true,
+                    'multiple' => true,
+                    'by_reference' => false,
+                ])
+            ->add('save', SubmitType::class, ['label' => 'Créer mon compte']);;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
